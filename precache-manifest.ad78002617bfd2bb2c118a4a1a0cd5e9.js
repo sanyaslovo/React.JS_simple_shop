427deaa5af811bc99c8c65869706e509af452bc5
sanyaslovo <sanyaslovo@gmail.com>
2020-11-17 12:33:00 +0200

    Updates

---
+++ precache-manifest.ad78002617bfd2bb2c118a4a1a0cd5e9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e43a51f1347a8d5cae4b1d84a3c9b1",
+    "url": "/react-ecommerse-homepage/index.html"
+  },
+  {
+    "revision": "64e2d18e3613eec94458",
+    "url": "/react-ecommerse-homepage/static/css/3.2d984c01.chunk.css"
+  },
+  {
+    "revision": "cb9e259cfb74f8f0cdda",
+    "url": "/react-ecommerse-homepage/static/css/4.0c8ecb65.chunk.css"
+  },
+  {
+    "revision": "156b9df0dfd33297e97d",
+    "url": "/react-ecommerse-homepage/static/css/5.f504232a.chunk.css"
+  },
+  {
+    "revision": "d4353fc63b5fbc1f8087",
+    "url": "/react-ecommerse-homepage/static/css/6.fe2871dc.chunk.css"
+  },
+  {
+    "revision": "ca8568f99e468534a616",
+    "url": "/react-ecommerse-homepage/static/css/7.0c630e50.chunk.css"
+  },
+  {
+    "revision": "50063b77878ecc62b15b",
+    "url": "/react-ecommerse-homepage/static/css/8.e917bbd7.chunk.css"
+  },
+  {
+    "revision": "52dda4a94b88437e3e2e",
+    "url": "/react-ecommerse-homepage/static/css/9.c33c69bd.chunk.css"
+  },
+  {
+    "revision": "fba6632afcbb6249d1c8",
+    "url": "/react-ecommerse-homepage/static/css/main.13820d97.chunk.css"
+  },
+  {
+    "revision": "4411882b216a8ae826b5",
+    "url": "/react-ecommerse-homepage/static/js/10.b6e1539b.chunk.js"
+  },
+  {
+    "revision": "ec53327e16b3e0966ac8",
+    "url": "/react-ecommerse-homepage/static/js/2.cfeb6974.chunk.js"
+  },
+  {
+    "revision": "64e2d18e3613eec94458",
+    "url": "/react-ecommerse-homepage/static/js/3.3f4e419a.chunk.js"
+  },
+  {
+    "revision": "cb9e259cfb74f8f0cdda",
+    "url": "/react-ecommerse-homepage/static/js/4.5aa784aa.chunk.js"
+  },
+  {
+    "revision": "156b9df0dfd33297e97d",
+    "url": "/react-ecommerse-homepage/static/js/5.dee0be78.chunk.js"
+  },
+  {
+    "revision": "d4353fc63b5fbc1f8087",
+    "url": "/react-ecommerse-homepage/static/js/6.6c299979.chunk.js"
+  },
+  {
+    "revision": "ca8568f99e468534a616",
+    "url": "/react-ecommerse-homepage/static/js/7.19de3da6.chunk.js"
+  },
+  {
+    "revision": "50063b77878ecc62b15b",
+    "url": "/react-ecommerse-homepage/static/js/8.8e84b824.chunk.js"
+  },
+  {
+    "revision": "52dda4a94b88437e3e2e",
+    "url": "/react-ecommerse-homepage/static/js/9.8e406455.chunk.js"
+  },
+  {
+    "revision": "fba6632afcbb6249d1c8",
+    "url": "/react-ecommerse-homepage/static/js/main.d74ab773.chunk.js"
+  },
+  {
+    "revision": "7baf91a64699ead09427",
+    "url": "/react-ecommerse-homepage/static/js/runtime~main.cfa56d4b.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/react-ecommerse-homepage/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/react-ecommerse-homepage/static/media/shopping-bag.09e6f5e7.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-ecommerse-homepage/precache-manifest.ad78002617bfd2bb2c118a4a1a0cd5e9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-ecommerse-homepage/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
