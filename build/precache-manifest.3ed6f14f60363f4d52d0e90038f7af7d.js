cb2348c1e59c74cc1abef310be8897c831c49a80
sanyaslovo <sanyaslovo@gmail.com>
2020-11-19 21:36:29 +0200

    Auto load to hosting

---
+++ build/precache-manifest.3ed6f14f60363f4d52d0e90038f7af7d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14b716b490369352b4a7b8ac0292a39a",
+    "url": "/react-ecommerse-homepage/index.html"
+  },
+  {
+    "revision": "4e3e845706bf9c9be884",
+    "url": "/react-ecommerse-homepage/static/css/3.8ae794c3.chunk.css"
+  },
+  {
+    "revision": "41bc4c39ab64a1cbcc8d",
+    "url": "/react-ecommerse-homepage/static/css/4.888a86be.chunk.css"
+  },
+  {
+    "revision": "7f9060e00a88bec5e268",
+    "url": "/react-ecommerse-homepage/static/css/5.385e2794.chunk.css"
+  },
+  {
+    "revision": "9ea5e39308bbdc2843c3",
+    "url": "/react-ecommerse-homepage/static/css/6.7c40d12b.chunk.css"
+  },
+  {
+    "revision": "91d78339b5f8754ac4df",
+    "url": "/react-ecommerse-homepage/static/css/7.66bfd6a1.chunk.css"
+  },
+  {
+    "revision": "8985f1ecb79ed65b637d",
+    "url": "/react-ecommerse-homepage/static/css/8.0f86c59f.chunk.css"
+  },
+  {
+    "revision": "484b6fab9d6f346143bd",
+    "url": "/react-ecommerse-homepage/static/css/9.b3bc0f52.chunk.css"
+  },
+  {
+    "revision": "5c50afb525aa00ca7375",
+    "url": "/react-ecommerse-homepage/static/css/main.93fb9dd5.chunk.css"
+  },
+  {
+    "revision": "4411882b216a8ae826b5",
+    "url": "/react-ecommerse-homepage/static/js/10.b6e1539b.chunk.js"
+  },
+  {
+    "revision": "ec53327e16b3e0966ac8",
+    "url": "/react-ecommerse-homepage/static/js/2.cfeb6974.chunk.js"
+  },
+  {
+    "revision": "4e3e845706bf9c9be884",
+    "url": "/react-ecommerse-homepage/static/js/3.e8c8b90c.chunk.js"
+  },
+  {
+    "revision": "41bc4c39ab64a1cbcc8d",
+    "url": "/react-ecommerse-homepage/static/js/4.cf07cee6.chunk.js"
+  },
+  {
+    "revision": "7f9060e00a88bec5e268",
+    "url": "/react-ecommerse-homepage/static/js/5.db0a34a0.chunk.js"
+  },
+  {
+    "revision": "9ea5e39308bbdc2843c3",
+    "url": "/react-ecommerse-homepage/static/js/6.e4062286.chunk.js"
+  },
+  {
+    "revision": "91d78339b5f8754ac4df",
+    "url": "/react-ecommerse-homepage/static/js/7.37d71769.chunk.js"
+  },
+  {
+    "revision": "8985f1ecb79ed65b637d",
+    "url": "/react-ecommerse-homepage/static/js/8.3b86248c.chunk.js"
+  },
+  {
+    "revision": "484b6fab9d6f346143bd",
+    "url": "/react-ecommerse-homepage/static/js/9.8e406455.chunk.js"
+  },
+  {
+    "revision": "5c50afb525aa00ca7375",
+    "url": "/react-ecommerse-homepage/static/js/main.3be119d4.chunk.js"
+  },
+  {
+    "revision": "5761d39879b1baee420a",
+    "url": "/react-ecommerse-homepage/static/js/runtime~main.523c29b7.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/react-ecommerse-homepage/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/react-ecommerse-homepage/static/media/shopping-bag.30f07071.svg"
+  }
+]);
